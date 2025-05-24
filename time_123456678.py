4f278401ed08aaf4f795a605197703a60107833c
kvali2 <avramchukmaksim29@gmail.com>
2025-05-24 13:25:29 +0300

    Update time_123456678.py

--- time_123456678.py
+++ time_123456678.py
@@ -1,8 +1,22 @@
 from datetime import datetime
+from dateutil.parser import parse
 
 # Завдання 1: Вивести поточну дату та час
 now = datetime.now()
 print(now.strftime("%d.%m.%Y %H:%M:%S"))
+
+
+# Завдання 4: Різниця між двома датами
+
+d1_str = input("Перша (01.01.0001) так напиши або не буду работать: ")
+d2_str = input("Друга (31.12.2025) так напиши або не буду работать: ")
+try:
+    d1 = parse(d1_str, dayfirst=True)
+    d2 = parse(d2_str, dayfirst=True)
+    delta = abs((d2 - d1).days)
+    print(f"Між тим і тим  пройшло {delta}  днів наверно сам пощитай")
+except:
+    print("неправильно роби правильно")
 
 
 # Завдання 5: Привітання залежно від часу доби
